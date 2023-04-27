3d1b9b721df8f7c6c9b5eccab64167a148c03d59
Guganhub <63343444+Guganhub@users.noreply.github.com>
2023-04-27 16:30:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(cookieParser())
 app.use(express.urlencoded({extended:false}))
 app.use(bodyParser.json())
 app.use(cors({
-    origin :['http://localhost:3000','https://inventoryapp.vercel.app'],
+    origin :['http://localhost:3000','https://inventory-app.vercel.app'],
     credentials:true
     })
 );
